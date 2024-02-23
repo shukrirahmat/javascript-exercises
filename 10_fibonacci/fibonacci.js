d4d68492f97f5fefd599699df84c8d005100ef65
Shukri Rahmat <119567950+shukrirahmat@users.noreply.github.com>
2024-02-24 03:28:07 +0800

    Finished with the rest of exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    number = parseInt(number);
 
+    if (number < 0) {
+        return "OOPS";
+    } else if (number === 0) {
+        return 0;
+    } else if (number === 1 || number === 2) {
+        return 1;
+    } else {
+        return fibonacci(number - 1) + fibonacci(number - 2);
+    }
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const date = new Date();
+    const thisYear = date.getFullYear();
+    return people
+        .reduce((oldest,current) => {
+                let oldestAge;
+                let currentAge;
+                if ('yearOfDeath' in oldest) {
+                    oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+                } else {
+                    oldestAge = thisYear - oldest.yearOfBirth;
+                }
+                if ('yearOfDeath' in current) {
+                    currentAge = current.yearOfDeath - current.yearOfBirth;
+                } else {
+                    currentAge = thisYear - current.yearOfBirth;
+                }
+                return currentAge > oldestAge? current : oldest;
+        })
 };
 
 // Do not edit below this line
